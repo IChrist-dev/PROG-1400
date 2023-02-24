060f9b29dc2c77398e5d1218c75a4e59566c06c6
Ian Christian <w0480449@nscc.ca>
2023-02-24 13:51:29 -0400

    Functionality achieved. Exercise Complete.

--- Exercises/Exercise - Take a Swing/src/TakeASwing.java
+++ Exercises/Exercise - Take a Swing/src/TakeASwing.java
@@ -6,8 +6,13 @@ w0480449
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
-public class TakeASwing extends JFrame{
+public class TakeASwing extends JFrame {
+
+    //Class-global variables for panel. Scoped for action listener methods
+    JPanel leftPanel;
+    JPanel rightPanel;
 
     //Constructor for the JFrame
     public TakeASwing() {
@@ -21,15 +26,19 @@ public class TakeASwing extends JFrame{
         //region Form Controls and panel/label properties
         JButton leftButton = new JButton("Toggle Left");
         leftButton.setBounds(100, 0, 200, 60);
+        //Apply functionality to the button
+        leftButton.addActionListener(this::leftToggleAction);
 
         JButton rightButton = new JButton("Toggle Right");
         rightButton.setBounds(500, 0, 200, 60);
+        //Apply functionality to the button
+        rightButton.addActionListener(this::rightToggleAction);
 
-        JPanel leftPanel = new JPanel();
+        leftPanel = new JPanel();
         leftPanel.setBounds(0, 60, 400, 340);
         leftPanel.setBackground(new Color(0, 0, 255));
 
-        JPanel rightPanel = new JPanel();
+        rightPanel = new JPanel();
         rightPanel.setBounds(400, 60, 400, 340);
         rightPanel.setBackground(new Color(255, 0, 0));
 
@@ -45,11 +54,9 @@ public class TakeASwing extends JFrame{
         rightMessage.setForeground(new Color(0, 0, 255));
         rightMessage.setFont(new Font("Sans Serif", Font.BOLD, 35));
 
-
         //Nest the messages within the coloured panels
         leftPanel.add(leftMessage);
         rightPanel.add(rightMessage);
-
         //endregion
 
         //region Controls added to container
@@ -59,8 +66,17 @@ public class TakeASwing extends JFrame{
         mainPage.add(rightButton);
         mainPage.add(leftPanel);
         mainPage.add(rightPanel);
-
         mainPage.setVisible(true);
         //endregion
+    }
+
+    //Action to toggle the left panel depending on its visibility state
+    public void leftToggleAction(ActionEvent e) {
+        leftPanel.setVisible(!leftPanel.isVisible());
+    }
+
+    //Action to toggle the right panel depending on its visibility state
+    public void rightToggleAction(ActionEvent e) {
+        rightPanel.setVisible(!rightPanel.isVisible());
     }
 }
